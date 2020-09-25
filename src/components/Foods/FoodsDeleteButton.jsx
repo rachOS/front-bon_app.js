cd6a6a12d954f58f0871770784941800e2d35c71
rachOS <g.chamekh@e-nautia.com>
2020-09-25 20:55:49 +0200

    fix bad spell of delete function

--- src/components/Foods/FoodsDeleteButton.jsx
+++ src/components/Foods/FoodsDeleteButton.jsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import Axios from "axios";
 
-function FoodsDeleteButton({getAllFoods, foodID}) {
+function FoodsDeleteButton({getFoodsList, foodID}) {
     const deleteFood = (foodID) => {
         const url = `${process.env.REACT_APP_HOST}/foods/${foodID}`;
-        Axios.delete(url, foodID).finally(() => getAllFoods());
+        Axios.delete(url, foodID).finally(() => getFoodsList());
     };
   return (
     <button onClick={() => deleteFood(foodID)}>X</button>
