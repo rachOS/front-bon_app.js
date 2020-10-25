7c6c64efb872997e200df7e30e8066071f9d28fe
rachOS <g.chamekh@e-nautia.com>
2020-10-25 14:38:11 +0100

    update: install http-proxy-middleware and move setpProxy to /src
    $ npm install http-proxy-middleware --save
    $ # or
    $ yarn add http-proxy-middleware
    
    https://create-react-app.dev/docs/proxying-api-requests-in-development

--- setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = (app) => {
-    app.use(proxy("/api/**", { target: "http://localhost:8000" }));
-    app.use(proxy("/otherApi/**", { target: "http://localhost:8000" }));
-};
---
+++ src/setupProxy.js
@@ -0,0 +1,6 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = (app) => {
+    app.use("/api/**", createProxyMiddleware({ target: "http://localhost:8000", changeOrigin: true }));
+    app.use("/ortherApi", createProxyMiddleware({ target: "http://localhost:8000", changeOrigin: true }));
+};
