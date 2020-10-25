7d40ce6bffb174667de71b8882aaa84eb98d210a
rachOS <g.chamekh@e-nautia.com>
2020-10-25 11:24:43 +0100

    build: create build with npm

---
+++ build/precache-manifest.084c9dae5b10d3265b6f122bee612d31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53464d38a5610529ff6f002c235a8e0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e252ebe20146d2f77ad4",
+    "url": "/static/css/main.34a91006.chunk.css"
+  },
+  {
+    "revision": "f9b364f5f87ae1773f3a",
+    "url": "/static/js/2.da8b6467.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.da8b6467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e252ebe20146d2f77ad4",
+    "url": "/static/js/main.02bc8d5a.chunk.js"
+  },
+  {
+    "revision": "6d1423ddff288417dfb4",
+    "url": "/static/js/runtime-main.3c414abc.js"
+  }
+]);
