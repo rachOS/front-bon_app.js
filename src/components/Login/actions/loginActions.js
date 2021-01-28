cd1dfa8dd4dc5f3873b4352a171e0785101a752d
rachOS <g.chamekh@e-nautia.com>
2021-01-28 21:41:56 +0100

    fix: clean code and delete redirection

--- src/components/Login/actions/loginActions.js
+++ src/components/Login/actions/loginActions.js
@@ -2,14 +2,13 @@
 import Axios from 'axios';
 
 const login = async (form) => {
-  console.log('ACTION', form);
   return await Axios.post(
     // eslint-disable-next-line no-undef
     `${process.env.REACT_APP_HOST}/auth/login`,
     form
-  ).then(async (response) => {
-    await response.data;
-    await location.assign('/accueil');
+  ).then((response) => {
+    const user = response.data;
+    return user;
   });
 };
 
