c95a075ecb18d2bc70cbd1ed86d85bfa111f6b45
rachOS <g.chamekh@e-nautia.com>
2021-02-07 19:32:11 +0100

    feature: button to redirect to <Login/>
    If user is not auth, display a button to redirect to the login page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,44 +1,50 @@
 /* eslint-disable indent */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Input, FieldSet, Label } from '../Form/Form';
+import { Form, Input, FieldSet, Label, Button } from '../Form/Form';
+import { Link } from 'react-router-dom';
 
 function Profile({ getUserDatas }) {
   const { user } = getUserDatas;
-  const userKey = Object.keys(user);
-  return (
+  const userKeys = Object.keys(user);
+  return user.id ? (
     <Form>
       <h1>Mes infos</h1>
-      {userKey &&
-        userKey.map((key, index) => {
-          switch (key) {
-            case 'id':
-              return;
-            case 'email':
-              return;
-            case 'password':
-              return;
-            default:
-              break;
-          }
-          return (
-            <FieldSet key={index}>
-              <Label text={key} htmlFor={user[key]} />
-              <Input
-                id={user[key]}
-                className="Form-input"
-                name={user[key]}
-                type={typeof user[key]}
-                onChange={(e) => {
-                  //  handle_form(e.target.name, e.target.value);
-                }}
-                placeholder={'JoDoehn'}
-                value={user[key]}
-                required
-              />
-            </FieldSet>
-          );
-        })}
+      {userKeys.map((key, index) => {
+        switch (key) {
+          case 'id':
+            return;
+          case 'email':
+            return;
+          case 'password':
+            return;
+          default:
+            break;
+        }
+        return (
+          <FieldSet key={index}>
+            <Label text={key} htmlFor={user[key]} />
+            <Input
+              id={user[key]}
+              className="Form-input"
+              name={user[key]}
+              type={typeof user[key]}
+              onChange={(e) => {
+                //  handle_form(e.target.name, e.target.value);
+              }}
+              placeholder={'JoDoehn'}
+              value={user[key]}
+              required
+            />
+          </FieldSet>
+        );
+      })}
+    </Form>
+  ) : (
+    <Form>
+      <Link to="/login">
+        <Button text="Login" />
+      </Link>
     </Form>
   );
 }
