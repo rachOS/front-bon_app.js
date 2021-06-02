33e05068219a8db8d9701bf02c46bea8496ea146
rachOS <g.chamekh@e-nautia.com>
2021-06-02 16:13:03 +0200

    [FIX] : display of Form in FoodsAdd. Add generic components (Label, Button, Input...)

--- src/adapters/primary/components/Foods/FoodsAdd.jsx
+++ src/adapters/primary/components/Foods/FoodsAdd.jsx
@@ -1,9 +1,7 @@
-// import core
 import React, { useState, useEffect, Fragment } from 'react';
 import Axios from 'axios';
 import PropTypes from 'prop-types';
-
-// get props from FoodsList.jsx
+import { FieldSet, Form, Legend, Label, Input, Button } from '../Form/Form';
 function FoodsAdd({ getFoodsList }) {
   const [food, setFood] = useState({});
   const [inputValue, setInputValue] = useState('');
@@ -14,7 +12,6 @@ function FoodsAdd({ getFoodsList }) {
     Axios.post(url, food).finally(() => getFoodsList());
   };
 
-  // get inputs values
   const handleChange = (event) => {
     event.preventDefault();
     const { name, value } = event.target;
@@ -27,100 +24,80 @@ function FoodsAdd({ getFoodsList }) {
     setFood(inputValue);
   }, [inputValue]);
 
-  // Si foodIndex est undefined => setInputValue(newValue) sinon setInputValue(oldValue)
-
-  // Todos? faire un composant form et passer les props
   return (
     <Fragment>
-      {/* Todo <Form values={values} legend id name type onChange htmlFor .../> */}
-      <form noValidate autoComplete="off">
-        <fieldset>
-          <legend>Ajouter un aliment</legend>
-          <label htmlFor="food name">
-            Name:
-            <input
-              id="food name"
-              name="name"
-              type="text"
-              value={inputValue.name}
-              onChange={(event) => handleChange(event)}
-            />
-          </label>
-          <label htmlFor="proteins">
-            {' '}
-            Protéines:
-            <input
-              id="proteins"
-              name="protein"
-              type="number"
-              value={inputValue.protein}
-              onChange={(event) => handleChange(event)}
-            />
-          </label>
-          <label htmlFor="lipids">
-            {' '}
-            Lipides:
-            <input
-              id="lipids"
-              name="lipid"
-              type="number"
-              value={inputValue.lipid}
-              onChange={(event) => handleChange(event)}
-            />
-          </label>
-          <label htmlFor="glucids">
-            {' '}
-            Glucides:
-            <input
-              id="glucids"
-              name="glucid"
-              type="number"
-              value={inputValue.glucid}
-              onChange={(event) => handleChange(event)}
-            />
-          </label>
-          <label htmlFor="brans">
-            {' '}
-            Fibres:
-            <input
-              id="brans"
-              name="bran"
-              type="number"
-              value={inputValue.bran}
-              onChange={(event) => handleChange(event)}
-            />
-          </label>
-          <label htmlFor="cals">
-            Calories:
-            <input
-              id="cals"
-              name="calories"
-              type="number"
-              value={inputValue.calories}
-              onChange={(event) => handleChange(event)}
-            />
-          </label>
-          <label htmlFor="category">
-            Catégorie:
-            <select
-              id="category"
-              name="id_group"
-              value={inputValue.id_group}
-              onChange={(event) => handleChange(event)}
-            >
-              <option value="">--Choisissez une catégorie--</option>
-              <option value={1}>féculent</option>
-              <option value={2}>animale</option>
-              <option value={3}>végétale</option>
-              <option value={4}>céréales</option>
-              <option value={5}>légumineuse</option>
-            </select>
-          </label>
-        </fieldset>
-      </form>
-      <button type="submit" onClick={() => addFood()}>
-        Ajouter
-      </button>
+      <Form noValidate autoComplete="off">
+        <FieldSet>
+          <Legend text={'Ajouter un aliment'} />
+          <Label htmlFor={'food name'} text={'Nom'} />
+          <Input
+            id="food name"
+            name="name"
+            type="text"
+            value={inputValue.name}
+            onChange={(event) => handleChange(event)}
+          />
+          <Label htmlFor="proteins" text={'Protéines'} />
+          <Input
+            id="proteins"
+            name="protein"
+            type="number"
+            value={inputValue.protein}
+            onChange={(event) => handleChange(event)}
+          />
+          <Label htmlFor="lipids" text={'Lipides'} />
+          <Input
+            id="lipids"
+            name="lipid"
+            type="number"
+            value={inputValue.lipid}
+            onChange={(event) => handleChange(event)}
+          />
+          <Label htmlFor="glucids" text={'Glucides'} />
+          <Input
+            id="glucids"
+            name="glucid"
+            type="number"
+            value={inputValue.glucid}
+            onChange={(event) => handleChange(event)}
+          />
+          <Label htmlFor="brans" text={'Fibres'} />
+          <Input
+            id="brans"
+            name="bran"
+            type="number"
+            value={inputValue.bran}
+            onChange={(event) => handleChange(event)}
+          />
+          <Label htmlFor="cals" text={'Calories'} />
+          <Input
+            id="cals"
+            name="calories"
+            type="number"
+            value={inputValue.calories}
+            onChange={(event) => handleChange(event)}
+          />
+          <Label htmlFor="category" text={'Catégorie'} />
+          <select
+            id="category"
+            name="id_group"
+            value={inputValue.id_group}
+            onChange={(event) => handleChange(event)}
+          >
+            <option value="">--Choisissez une catégorie--</option>
+            <option value={1}>féculent</option>
+            <option value={2}>animale</option>
+            <option value={3}>végétale</option>
+            <option value={4}>céréales</option>
+            <option value={5}>légumineuse</option>
+          </select>
+        </FieldSet>
+      </Form>
+      <Button
+        type="submit"
+        text={'Ajouter un aliment'}
+        onClick={() => addFood()}
+      />
     </Fragment>
   );
 }
